9e4787e817e8045b6b41b5825f9cfa352ce8b49d
tralalaoh <128903195+tralalaoh@users.noreply.github.com>
2025-11-22 02:32:47 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Turkish123CloudstreamPlugins"
+
+// This file sets what projects are included. Every time you add a new project, you must add it
+// to the includes below.
+
+// Plugins are included like this
+include("Turkish123Provider")
